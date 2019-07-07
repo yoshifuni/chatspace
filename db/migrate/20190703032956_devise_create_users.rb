e38da643673ec6d0dac06f50ff201f2267f140aa
yoshifumi <yosshan.a.yoshi@i.softbank.jp>
2019-07-07 10:35:27 +0900

    グループのモデルとコントローラの作成

--- db/migrate/20190703032956_devise_create_users.rb
+++ db/migrate/20190703032956_devise_create_users.rb
@@ -4,16 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, unique: true, index: true
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false, unique: true, 
+      
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
+      
       ## Rememberable
-      t.datetime :remember_created_at
+      
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
@@ -34,7 +31,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps 
     end
 
     add_index :users, :email,                unique: true
