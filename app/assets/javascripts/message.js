9e76ef38ef40d63de890ecd83844516b24aaebcc
yoshifumi <yosshan.a.yoshi@i.softbank.jp>
2019-07-17 12:31:03 +0900

    chatspaceメッセージ機能非同期化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,53 @@
+$(function(){
+
+
+  function buildPost(post){
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">
+                      ${post.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${post.date}
+                    </div>
+                  </div>
+                  <div class="message__lower-message">
+                    <p class="messasge__lower-message__content">
+                      ${post.content}
+                    </p>
+                  </div>
+                </div>`
+    
+    return html;
+  }
+  
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(post){
+      var html = buildPost(post);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      $('.form__submit').prop('disabled', false);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      
+
+
+    })
+    .fail(function(){
+      alert('エラー');
+    })
+    
+  })
+})
