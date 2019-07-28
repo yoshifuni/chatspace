90bf10c8013a97a02fe48af830cc101a1e7e35c1
yoshifumi <yosshan.a.yoshi@i.softbank.jp>
2019-07-28 17:51:34 +0900

    chatspace.deploy.rb.アップリケーション名の-を削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '~> 3.11.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space'
+set :application, 'chatspace'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:yoshifuni/chatspace.git'
